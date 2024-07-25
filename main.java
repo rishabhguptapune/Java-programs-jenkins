2fa7f6192e57fed37cdb6c166ef3f284c03e87e1
rishabhguptapune <rishabh.gupta.pune@gmail.com>
2024-07-26 00:47:45 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class main {
+    
+    public static void main(String[] args) {
+        System.out.println("this is a java program");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+        System.out.println("Hello 1");
+    }
+}
